e99047f9ffbbaeb512de21bf2458fb6dd7d80bd7
braedynl <braedynlettinga@gmail.com>
2023-09-28 17:29:32 -0400

    Allow optional `capacity`

--- channels/core.py
+++ channels/core.py
@@ -4,7 +4,7 @@ __all__ = ["Channel"]
 
 import asyncio
 from collections import deque as Deque
-from typing import Generic, TypeVar
+from typing import Generic, TypeVar, Optional
 
 from .protocols import SupportsRecvAndSend
 
@@ -16,17 +16,16 @@ class Channel(SupportsRecvAndSend[T, T], Generic[T]):
     __slots__ = ("_values")
     _values: Deque[T]
 
-    def __init__(self, capacity: int = -1) -> None:
-        self._values = Deque((), None if capacity < 0 else capacity)
+    def __init__(self, capacity: Optional[int] = None) -> None:
+        self._values = Deque((), capacity)
 
     def __len__(self) -> int:
         return len(self._values)
 
     @property
-    def capacity(self) -> int:
+    def capacity(self) -> Optional[int]:
         """The channel's maximum possible size"""
-        capacity = self._values.maxlen
-        return -1 if capacity is None else capacity
+        return self._values.maxlen
 
     def full(self) -> bool:
         """Return true if the channel has reached its capacity, otherwise false"""
